b5d1effcaac2ba642f276be79c00ed5fc8362aea
Luuk Holleman <luukholleman@gmail.com>
2015-06-11 20:47:43 +0200

    Added cursor lock

--- Assets/Code/GUI/CrossHair.cs
+++ Assets/Code/GUI/CrossHair.cs
@@ -7,6 +7,22 @@ namespace Assets.Code.GUI
         public Texture2D CrosshairTexture;
         public float CrosshairScale = 1;
 
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+
         void OnGUI()
         {
             if (CrosshairTexture != null)
